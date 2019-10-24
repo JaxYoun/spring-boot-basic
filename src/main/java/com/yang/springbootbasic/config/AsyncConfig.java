5ee4f1748f8e88fc424824ae093f56c39fac8475
JaxYoun <gao88jie@qq.com>
2019-10-24 10:56:17 +0800

    异步线程池配置补充

--- src/main/java/com/yang/springbootbasic/config/AsyncConfig.java
+++ src/main/java/com/yang/springbootbasic/config/AsyncConfig.java
@@ -10,10 +10,23 @@ public class AsyncConfig {
     
     public ThreadPoolTaskExecutor AsyncThreadPoolTaskExecutor() {
       ThreadPoolTaskExecutor pool = new ThreadPoolTaskExecutor();
+      
+      1.配置线程池静态参数
       pool.setCorePoolSize(CORE_SIZE);
       pool.setMaxPoolSize(MAX_SIZE)
       pool.setQueueCapacity(QUEUE_CAPACITY);
       pool.setKeepAliveSeconds(KEEP_ALIVE_SECOND);
       pool.setThreadNamePrefix(THREAD_PREFIX);
+      
+      2.配置拒绝策略
+      pool.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+      3.等待池内任务完成才关闭池
+      pool.setWaitForTaskToCompleteOnShutdown(true);
+      4.配置池关闭前的等待时间
+      pool.setAwaitTerminationSeconds(60);
+      pool.initialize();
+      
+      log.info("async thread pool has initialized!");
+      return pool;
     }
 }
